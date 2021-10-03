d8c0e36bddb8f29856e6b7efad7e039c5ee0902b
Ben Kirk <benjaminbarriekirk@gmail.com>
2021-10-03 21:32:30 +0700

    multithreading and list of transcription jobs

--- gui.py
+++ gui.py
@@ -1,7 +1,12 @@
 import sys
-import os 
-from PyQt5.QtWidgets import QApplication, QComboBox, QFileDialog, QHBoxLayout, QLineEdit, QPushButton, QTableView, QVBoxLayout, QWidget, QLabel
+import os
+import tempfile
+from PyQt5.QtCore import QThreadPool 
+from PyQt5.QtWidgets import QApplication, QComboBox, QFileDialog, QHBoxLayout, QLineEdit, QProgressBar, QPushButton, QTableView, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, QLabel
 import json
+from main import Transcriber
+import time
+import multi_threading
 # from PyQt5 import QtCore
 
 
@@ -9,48 +14,57 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Speech to Text Program")
-        self.setGeometry(500, 500,500,500)
-        self.audio_formats = [".mp3",".wav", ".ogg",".wma",".flac",".mp4",".aac"]
+        self.setGeometry(500, 500,1000,500)
+        self.audio_formats = [".mp3",".wav", ".ogg",".wma",".flac",".mp4",".aac",".m4a"]
         self.video_formats = [".mp4",".mov",".wmv",".avi"]
-        # self.audio_formats = "*.mp3 *.wav *.ogg *.wma *.flac *.m4a *.mp4 *.aac"
-        # self.video_formats = "*.mp4 *.mov *.wmv *.avi"
         self.set_up_ui()
         self.show()
+        self.transcriber = Transcriber()
     
     def set_up_ui(self):
 
         with open("lang_support.json") as f:
-            lang_dict = json.load(f)
+            self.lang_dict = json.load(f)
 
         lang_lb = QLabel("Language: ")
-        lang_combo = QComboBox()
-        lang_combo.addItems(lang_dict.keys())
+        self.lang_combo = QComboBox()
+        self.lang_combo.addItems(self.lang_dict.keys())
         sub_layout = QHBoxLayout()
         sub_layout.addWidget(lang_lb)
-        sub_layout.addWidget(lang_combo)
+        sub_layout.addWidget(self.lang_combo)
 
-        self.indivdual_files_btn = QPushButton("Select Indivdual Audio Files")
-        self.folder_btn = QPushButton("Select Folder (all files in folder)")
+        self.indivdual_files_btn = QPushButton("Transcribe Indivdual Audio Files")
+        self.folder_btn = QPushButton("Transcribe Folder (all files in folder)")
 
-        job_view = QTableView()
+        self.job_view = QTableWidget()
+        self.job_view.setColumnCount(5)
+        self.job_view.setRowCount(1)
+        self.job_view.setCellWidget(0,0,QLabel("Name"))
+        self.job_view.setCellWidget(0,1,QLabel("Loading Bar"))
+        self.job_view.setCellWidget(0,2,QLabel("Status"))
+        self.job_view.setCellWidget(0,3,QLabel("Length"))
+        self.job_view.setCellWidget(0,4,QLabel("Transcription Time"))
+
+
 
         layout = QVBoxLayout()
         layout.addLayout(sub_layout)
         layout.addWidget(self.indivdual_files_btn)
         layout.addWidget(self.folder_btn)
-        layout.addWidget(job_view)
+        layout.addWidget(self.job_view)
         self.setLayout(layout)
 
         #connections
         self.indivdual_files_btn.clicked.connect(self.select_individual_files)
         self.folder_btn.clicked.connect(self.select_folder)
 
+        self.threadpool = QThreadPool()
+
     def select_individual_files(self):
         # options = QFileDialog.Options()
         # options |= QFileDialog.DontUseNativeDialog
         audio_format_with_asterisk = " *" + " *".join(self.audio_formats)
         video_format_with_asterisk = " *" + " *".join(self.video_formats)
-        print(audio_format_with_asterisk)
         files, _ = QFileDialog.getOpenFileNames(
             self,
             "Select Individual Audio Files (use ctrl or shift to select more than one file)",
@@ -58,28 +72,108 @@ class MainWindow(QWidget):
             f"Audio Files ({audio_format_with_asterisk});;Video Files ({video_format_with_asterisk});",
             # options=options
             )
+        if files:
+            self.start_threading(files)
 
     def select_folder(self):
         path = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
         if path:
             files = self.find_all_files_in_folder(path)
+            if files != []:
+                self.start_threading(files)
     
     def find_all_files_in_folder(self,path):
         list_of_files = []
         if os.path.exists(path):
             temp_list = os.listdir(path)
             for file in temp_list:
+                full_path = os.path.join(path,file)
                 file_type = os.path.splitext(file)[1]
-                print(file_type)
                 if file_type in self.audio_formats:
-                    list_of_files.append(file)
-                if file_type in self.video_formats:
-                    list_of_files.append(file)
+                    list_of_files.append(full_path)
+                elif file_type in self.video_formats:
+                    list_of_files.append(full_path)
+        print(list_of_files)
         return list_of_files
 
+    def start_threading(self,files):
+        for file in files:
+            lang = self.lang_dict[self.lang_combo.currentText()]
+            worker = multi_threading.Worker(self.process_file,file,lang,self.job_view.rowCount())
+            worker.signals.progress.connect(self.update_table_view)
+            # worker.signals.result.connect(self.nothing)
+            # worker.signals.finished.connect(self.nothing)
+            self.threadpool.start(worker)
+            print(f"thread count is {self.threadpool.activeThreadCount()}")
+
+            self.job_view.setRowCount(self.job_view.rowCount() + 1)
+
+
+                
+    def process_file(self, file, lang, index_in_table, progress_callback):
+            start_time = time.perf_counter()
+            
+            progress_callback.emit({"filename":file,"file_length":"","chunks":10,"current_chunk":0,"index_in_table":index_in_table ,"time_taken":0})
+            with tempfile.TemporaryDirectory() as tempdir:
+                ori_file_length_from_ffmpeg = self.transcriber.split_into_smaller_chunks(tempdir, file)
+                if ori_file_length_from_ffmpeg is not None:
+                    temp_file_names = os.listdir(tempdir)
+                    full_paths = [os.path.join(tempdir,f) for f in temp_file_names]
+                    dir_path, filename = os.path.split(file)
+
+                    # clean up file name
+                    for format in self.audio_formats:
+                        if format in filename:
+                            print(format)
+                            clean_filename = filename.replace(format,"")
+                            print(filename)
+                        else:
+                            for format in self.video_formats:
+                                if format in filename:
+                                    clean_filename = filename.replace(format,"")                    
+
+                    if os.path.exists(file):
+                        clean_filename = clean_filename + " (new)"
+
+                    txt_full_path = os.path.join(dir_path, clean_filename)
+
+                    for i, temp_file in enumerate(full_paths):
+                        trans_results = self.transcriber.transcribe_temp_audio_file(temp_file,lang)
+                        chunk_time = time.perf_counter()
+                        time_taken = chunk_time - start_time
+                        the_dict = {"filename":file,"file_length":ori_file_length_from_ffmpeg,"chunks":len(full_paths),"current_chunk":i+1,"index_in_table":index_in_table,"time_taken":time_taken}
+                        progress_callback.emit(the_dict)
+                        if trans_results != None:
+                            self.transcriber.write_to_txt(txt_full_path + ".txt",trans_results)
+                        else:
+                            self.transcriber.write_to_txt(txt_full_path + ".txt"," ? ")
+
+
+
     
+    def update_table_view(self,update):
+        filename = os.path.split(update["filename"])[1]
+        file_length = update["file_length"]
+        chunks = update["chunks"]
+        current_chunk = update["current_chunk"]
+        index_in_job_view = update["index_in_table"]
+        time_taken = update["time_taken"]
 
+        progressbar = QProgressBar()
+        progressbar.setMaximum(chunks)
+        progressbar.setValue(current_chunk)
 
+        if current_chunk == chunks:
+            status = "DONE"
+        else:
+            status = "IN PROGRESS"
+        
+        self.job_view.setCellWidget(index_in_job_view,0,QLabel(filename))
+        self.job_view.setCellWidget(index_in_job_view,1,progressbar)
+        self.job_view.setCellWidget(index_in_job_view,2,QLabel(status))
+        self.job_view.setCellWidget(index_in_job_view,3,QLabel(file_length))
+        self.job_view.setCellWidget(index_in_job_view,4,QLabel(str(time_taken)))
+        
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- main.py
+++ main.py
@@ -6,135 +6,141 @@ import speech_recognition as sr
 import time
 import re 
 
-def get_audio_files():
-    "Returns a list of all audio files in current working directory"
-    files_in_cwd = os.listdir(os.getcwd())
-    return [f for f in files_in_cwd if pathlib.Path(f).suffix.lower() in [".mp3",".wav",".ogg",".mp4",".m4a"]]
+class Transcriber:
 
-def split_into_smaller_chunks(tempdir, audio_file):
-    "Split up audio using ffmpeg and save to temp directory"
-    output_file = os.path.join(tempdir,audio_file.split(".")[0])
-    # process = sp.Popen(f'ffmpeg -i "{audio_file}" -f segment -segment_time 150 "{output_file}"%03d.wav"',stdout=sp.PIPE, stderr=sp.PIPE)
-    process = sp.Popen(
-            [
-            "ffmpeg","-i",
-            audio_file,
-            "-f", "segment", "-segment_time", "150",
-            f"{output_file}%03d.wav"
-            ],
-            stdout=sp.PIPE,
-            stderr=sp.PIPE
-            )
+    def get_audio_files(self):
+        "Returns a list of all audio files in current working directory"
+        files_in_cwd = os.listdir(os.getcwd())
+        return [f for f in files_in_cwd if pathlib.Path(f).suffix.lower() in [".mp3",".wav",".ogg",".mp4",".m4a"]]
 
-    try:
-        stdout,stderr = process.communicate()
-        pattern = re.compile(r"(?<=Duration:\s)\d+:\d+:\d+")
-        length = re.match(pattern, stdout.decode())
-        if length is None:
-            length = re.search(pattern, stderr.decode())
-        return length.group(0)
-    except sp.TimeoutExpired as e:
-        print(f"Timed out while tring to split up this file:{audio_file}")
-        print(e)
-        return None
+    def split_into_smaller_chunks(self,tempdir, audio_file):
+        "Split up audio using ffmpeg and save to temp directory"
+        output_file = os.path.join(tempdir,"audio")
+        process = sp.Popen(
+                [
+                "ffmpeg","-i",
+                audio_file,
+                "-f", "segment", "-segment_time", "150",
+                f"{output_file}%03d.wav"
+                ],
+                stdout=sp.PIPE,
+                stderr=sp.PIPE
+                )
 
-def transcribe_temp_audio_file(file):
-    "transcribe audio clip using google dev api"
-    with sr.AudioFile(file) as source:
-        r = sr.Recognizer()
         try:
-            data = r.record(source)
-            response = r.recognize_google(data,language="id-ID")
-        except sr.UnknownValueError as e:
-            print(f"UNKNOWN VALUE ERROR {e}")
+            stdout,stderr = process.communicate()
+            pattern = re.compile(r"(?<=Duration:\s)\d+:\d+:\d+")
+            length = re.match(pattern, stdout.decode())
+            if length is None:
+                length = re.search(pattern, stderr.decode())
+            try:
+                return length.group(0)
+            except:
+                return length
+
+        except sp.TimeoutExpired as e:
+            print(f"Timed out while tring to split up this file:{audio_file}")
+            print(e)
             return None
-        except sr.RequestError as e:
-            print(f"REQUEST ERROR {e}")
-            return None
-        except Exception as e:
-            print(f"ERROR FROM GOOGLE {e}")
-            # answer = input('DO YOU WANT TO TRY AGAIN (y) or MOVE ON TO ANOTHER PART OF THE AUDIO (n)?')
-            # if answer.lower().strip() == 'y':
-            #     transcribe_temp_audio_file(file)
-            # # elif answer.lower().strip() == 'n':
-            # else:
-            return None
-        return response
 
-def write_to_txt(txt_file_name,text):
-    "append to txt file"
-    with open(txt_file_name,"a",encoding="utf-8") as txt_file:
-        txt_file.write("\n" + text)
+    def transcribe_temp_audio_file(self, file,lang):
+        "transcribe audio clip using google dev api"
+        with sr.AudioFile(file) as source:
+            r = sr.Recognizer()
+            try:
+                data = r.record(source)
+                response = r.recognize_google(data,language=lang)
+            except sr.UnknownValueError as e:
+                print(f"UNKNOWN VALUE ERROR {e}")
+                return None
+            except sr.RequestError as e:
+                print(f"REQUEST ERROR {e}")
+                return None
+            except Exception as e:
+                print(f"ERROR FROM GOOGLE {e}")
+                # answer = input('DO YOU WANT TO TRY AGAIN (y) or MOVE ON TO ANOTHER PART OF THE AUDIO (n)?')
+                # if answer.lower().strip() == 'y':
+                #     transcribe_temp_audio_file(file)
+                # # elif answer.lower().strip() == 'n':
+                # else:
+                return None
+            return response
 
-def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):# 
-    """
-    Call in a loop to create terminal progress bar
-    @params:
-        iteration   - Required  : current iteration (Int)
-        total       - Required  : total iterations (Int)
-        prefix      - Optional  : prefix string (Str)
-        suffix      - Optional  : suffix string (Str)
-        decimals    - Optional  : positive number of decimals in percent complete (Int)
-        length      - Optional  : character length of bar (Int)
-        fill        - Optional  : bar fill character (Str)
-        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
-    """
-    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + '-' * (length - filledLength)
-    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
-    # Print New Line on Complete
-    if iteration == total: 
-        pass
-        # print("                                                         ")
+    def write_to_txt(self, txt_file_name,text):
+        "append to txt file"
+        with open(txt_file_name,"a",encoding="utf-8") as txt_file:
+            txt_file.write("\n" + text)
 
-def print_files_found(audio_files):
-    "Prints audio file names to console"
-    print(f"{len(audio_files)} FILES FOUND:")
+    def printProgressBar (self, iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):# 
+        """
+        Call in a loop to create terminal progress bar
+        @params:
+            iteration   - Required  : current iteration (Int)
+            total       - Required  : total iterations (Int)
+            prefix      - Optional  : prefix string (Str)
+            suffix      - Optional  : suffix string (Str)
+            decimals    - Optional  : positive number of decimals in percent complete (Int)
+            length      - Optional  : character length of bar (Int)
+            fill        - Optional  : bar fill character (Str)
+            printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+        """
+        percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+        filledLength = int(length * iteration // total)
+        bar = fill * filledLength + '-' * (length - filledLength)
+        print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+        # Print New Line on Complete
+        if iteration == total: 
+            pass
+            # print("                                                         ")
 
-    for i, f in enumerate(audio_files):
-        print( f"{i+1}. " + f)
-    print('-'*50)
+    def print_files_found(self, audio_files):
+        "Prints audio file names to console"
+        print(f"{len(audio_files)} FILES FOUND:")
 
-def main():
-    "main function that calls other functions"
-    start_time = time.perf_counter()
-    audio_files = get_audio_files()
-    print_files_found(audio_files)
+        for i, f in enumerate(audio_files):
+            print( f"{i+1}. " + f)
+        print('-'*50)
 
-    for audio_file in audio_files:
-        start_time_for_file =time.perf_counter()
-        with tempfile.TemporaryDirectory() as tempdir:
-            ori_length = split_into_smaller_chunks(tempdir,audio_file)
-            if ori_length != None:
-                temp_file_names = os.listdir(tempdir)
-                full_paths = [os.path.join(tempdir,f) for f in temp_file_names]
-                txt_file_name = audio_file.split(".")[0] + ".txt"
-                printProgressBar(0,len(full_paths),prefix="TRANSCRIBING",suffix=audio_file,length=50,printEnd="\r")
-                for i, file in enumerate(full_paths):
-                    trans_results = transcribe_temp_audio_file(file)
-                    while trans_results == None:
-                        print("\a") # make alert noise
-                        print("CHECK YOUR INTERNET CONNECTION")
-                        answer = input('DO YOU WANT TO TRY AGAIN (y) or MOVE ON TO ANOTHER PART OF THE AUDIO (n)?')
-                        if answer.lower().strip() == 'y' or answer.lower().strip() == 'yes':
-                            trans_results = transcribe_temp_audio_file(file)
-                        else:
-                            trans_results = " |--THIS PART FAILED TO TRANSCRIBE (MAY HAVE LOST INTERNET)--| "
-                            break
-                    printProgressBar(i+1,len(full_paths),prefix="TRANSCRIBING",suffix=audio_file,length=50,printEnd = "\r")
-                    write_to_txt(txt_file_name,trans_results)
-                end_time_for_file = time.perf_counter()
-                time_taken_for_file = time.gmtime(end_time_for_file-start_time_for_file)
-                print(f'DONE | ORIGINAL FILE LENGTH {ori_length} | TIME TAKEN TO TRANSCRIBE {time.strftime("%H:%M:%S",time_taken_for_file)} | FILE NAME {audio_file}')
-            else:
-                print(f' FAILED | FILE NAME "{audio_file}"')
+    def simple_cli(self, lang):
+        "main function that calls other functions"
+        start_time = time.perf_counter()
+        audio_files = self.get_audio_files()
+        self.print_files_found(audio_files)
 
-    end_time = time.perf_counter()
-    time_taken = time.gmtime(end_time - start_time)
-    print("Total time to complete all files: " + time.strftime("%H:%M:%S",time_taken))
-    print("\a") # make alert noise
+        for audio_file in audio_files:
+            start_time_for_file =time.perf_counter()
+            with tempfile.TemporaryDirectory() as tempdir:
+                ori_length = self.split_into_smaller_chunks(tempdir,audio_file)
+                if ori_length != None:
+                    temp_file_names = os.listdir(tempdir)
+                    full_paths = [os.path.join(tempdir,f) for f in temp_file_names]
+                    txt_file_name = audio_file.split(".")[0] + ".txt"
+                    self.printProgressBar(0,len(full_paths),prefix="TRANSCRIBING",suffix=audio_file,length=50,printEnd="\r")
+                    for i, file in enumerate(full_paths):
+                        trans_results = self.transcribe_temp_audio_file(file, lang)
+                        while trans_results == None:
+                            print("\a") # make alert noise
+                            print("CHECK YOUR INTERNET CONNECTION")
+                            answer = input('DO YOU WANT TO TRY AGAIN (y) or MOVE ON TO ANOTHER PART OF THE AUDIO (n)?')
+                            if answer.lower().strip() == 'y' or answer.lower().strip() == 'yes':
+                                trans_results = self.transcribe_temp_audio_file(file,lang)
+                            else:
+                                trans_results = " |--THIS PART FAILED TO TRANSCRIBE (MAY HAVE LOST INTERNET)--| "
+                                break
+                        self.printProgressBar(i+1,len(full_paths),prefix="TRANSCRIBING",suffix=audio_file,length=50,printEnd = "\r")
+                        self.write_to_txt(txt_file_name,trans_results)
+                    end_time_for_file = time.perf_counter()
+                    time_taken_for_file = time.gmtime(end_time_for_file-start_time_for_file)
+                    print(f'DONE | ORIGINAL FILE LENGTH {ori_length} | TIME TAKEN TO TRANSCRIBE {time.strftime("%H:%M:%S",time_taken_for_file)} | FILE NAME {audio_file}')
+                else:
+                    print(f' FAILED | FILE NAME "{audio_file}"')
+
+        end_time = time.perf_counter()
+        time_taken = time.gmtime(end_time - start_time)
+        print("Total time to complete all files: " + time.strftime("%H:%M:%S",time_taken))
+        print("\a") # make alert noise
 
 
 if __name__ == "__main__":
-    main()
+    transcriber = Transcriber()
+    transcriber.simple_cli(lang="id-ID")
